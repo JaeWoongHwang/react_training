d6b0a65580d2a047be7018d8a20a74ffa70f3b14
JaeWoongHwang <jwngh15@gmail.com>
2018-07-27 11:40:40 +0900

    Today's training

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,27 +2,50 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Inception",
-  "Fight Club",
-  "Antman and Wasp",
-  "Godfather2"
+const movie = [
+  {
+    title:"Inception",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/51ShRC1YMrL.jpg"
+  },
+  {
+    title: "Fight Club",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/5190cUTcbZL._SY445_.jpg"
+  },
+  {
+
+    title: "Antman and Wasp",
+    poster: "http://www.joblo.com/posters/images/full/antman_and_the_wasp_new.jpg"
+  },
+  {
+    title: "Godfather part2",
+    poster:  "https://i.pinimg.com/736x/7d/ec/20/7dec20321ec4bfa6ee704cb20f759d02--the-godfather-poster-godfather-.jpg"
+  }
 ]
 
-const movieImages = [
-  "https://images-na.ssl-images-amazon.com/images/I/51ShRC1YMrL.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/5190cUTcbZL._SY445_.jpg",
-  "http://www.joblo.com/posters/images/full/antman_and_the_wasp_new.jpg",
-  "https://i.pinimg.com/736x/7d/ec/20/7dec20321ec4bfa6ee704cb20f759d02--the-godfather-poster-godfather-.jpg"
-]
+
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+  state = {
+    greeting : "Welcome to Movie Chart!"
+  }
+  
+  
+  componentDidMount(){
+    setTimeout(()=> {
+      this.setState({
+        greeting: "Enjoy Our Service"
+      })
+    }, 2000)
+  }
+
   render() {
     return (
       <div className="App">
-        <Movie title = {movieTitles[0]} poster = {movieImages[0]}/>
-        <Movie title = {movieTitles[1]} poster = {movieImages[1]}/>
-        <Movie title = {movieTitles[2]} poster = {movieImages[2]}/>
-        <Movie title = {movieTitles[3]} poster = {movieImages[3]}/>
+      {this.state.greeting}
+      {movie.map((movie, index) =>{
+        return <Movie title={movie.title} poster={movie.poster} key={index} />
+      })}
       </div>
     );
   }
